f528519f4f62b3f6491c6317f5a3f3b4c57dad14
Samuel Andrade <devsamuelandrade@gmail.com>
2021-11-25 22:23:53 -0300

    Create flutter app and add main file

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const PersonalExpensesApp());
+
+class PersonalExpensesApp extends StatelessWidget {
+  const PersonalExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: const Center(
+        child: Text("Versão inicial"),
+      ),
+    );
+  }
+}
