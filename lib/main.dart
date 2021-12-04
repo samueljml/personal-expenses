03b91cf5f845e57e70072701898e88bbe520b6ee
Samuel Andrade <devsamuelandrade@gmail.com>
2021-12-04 09:39:49 -0300

    Add add new expanse form

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,6 @@ class MyHomePage extends StatelessWidget {
           title: const Text("Despesas Pessoais"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -97,6 +96,36 @@ class MyHomePage extends StatelessWidget {
                   ),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: "Título",
+                        ),
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(labelText: "valor (R\$)"),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text("Nova Transação"),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
             )
           ],
         ));
