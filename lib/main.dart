1343cd16fd5d254ba547edde8d3cc61ca07151bd
Samuel Andrade <devsamuelandrade@gmail.com>
2021-12-18 22:19:57 -0300

    Add Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -24,19 +24,21 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Despesas Pessoais"),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: const Card(
-                child: Text("Gráfico"),
-                elevation: 5,
-                color: Colors.blue,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: const Card(
+                  child: Text("Gráfico"),
+                  elevation: 5,
+                  color: Colors.blue,
+                ),
               ),
-            ),
-            const TransactionUser()
-          ],
+              const TransactionUser()
+            ],
+          ),
         ));
   }
 }
