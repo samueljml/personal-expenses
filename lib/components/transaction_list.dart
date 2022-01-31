3dcf175b9eb9089f5130ab67d40baf7b4a556c92
Samuel Andrade <devsamuelandrade@gmail.com>
2022-01-31 18:09:18 -0300

    Add responsive transaction list empty message

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -13,27 +13,32 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Nenhuma Transação Cadastrada",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  "assets/images/waiting.png",
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (context, constraints) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: constraints.maxHeight * 0.05,
                 ),
-              ),
-            ],
-          )
+                SizedBox(
+                  height: constraints.maxHeight * 0.3,
+                  child: Text(
+                    "Nenhuma Transação Cadastrada",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.05,
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    "assets/images/waiting.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
