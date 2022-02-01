accb98a72f4a95d75aed8319facbd827d6e01648
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-01 18:30:21 -0300

    Refactor using variable to mediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -87,13 +87,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text(
         "Despesas Pessoais",
-        style: TextStyle(fontSize: 20 * MediaQuery.of(context).textScaleFactor),
+        style: TextStyle(fontSize: 20 * mediaQuery.textScaleFactor),
       ),
       actions: [
         if (isLandscape)
@@ -110,8 +111,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availableHeight = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).padding.top -
+    final availableHeight = mediaQuery.size.height -
+        mediaQuery.padding.top -
         appBar.preferredSize.height;
 
     return Scaffold(
