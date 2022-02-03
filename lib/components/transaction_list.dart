cab64d6251910e3b9b0f513822077e3bf42bd3e6
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-03 18:26:58 -0300

    Extract component item from transaction list

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses/components/transaction_item.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -40,47 +41,9 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('R\$${tr.value}'),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                  trailing: MediaQuery.of(context).size.width > 400
-                      ? TextButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(
-                            Icons.delete,
-                            color: Colors.grey,
-                          ),
-                          label: const Text(
-                            "Excluir",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        )
-                      : TextButton(
-                          onPressed: () => onRemove(tr.id),
-                          child: const Icon(
-                            Icons.delete,
-                            color: Colors.grey,
-                          ),
-                        ),
-                ),
+              return TransactionItem(
+                tr: tr,
+                onRemove: onRemove,
               );
             },
           );
